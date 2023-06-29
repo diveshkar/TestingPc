fbcda901c6dac74433429a0b388ef34437a7a2e3
Nizara <67256107+Nishara-Sewminie@users.noreply.github.com>
2023-06-29 16:46:57 +0530

    Add files via upload

--- frontend/src/Component/Login.js
+++ frontend/src/Component/Login.js
@@ -1,44 +1,97 @@
 import React from 'react'
 import './login.css';
-import axios from 'axios';
+import logImg from './images/login.jpg';
+import {Link} from 'react-router-dom';
 import { useState } from 'react';
+import axios from 'axios';
 
-function Login () {
-  const [formData, setFormData] = useState({
-    username_or_Email:'',
-    password:''
+function Login ()  {
+
+    const [formData, setFormData] = useState({
+      username_or_Email:'',
+      password:''
+      
+    });
+  
+    const handleChange = (e) => {
+      setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      let url = 'http://localhost/testingpc/backend/login.php';
+      axios.post(url , formData).then(function(response) {
+        if(response.data.success) {
+          window.location.href = '/order';
+          alert("login succed");
+        }
+      })
+      .catch(function(error){
+        alert(error);
+      })
+      
+    }
+  
+
     
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    let url = 'http://localhost/testingpc/backend/login.php';
-    axios.post(url , formData).then(function(response) {
-      if(response.data.success) {
-        window.location.href = '/order';
-        alert("login succed");
-      }
-    })
-    .catch(function(error){
-      alert(error);
-    })
-    
-  }
-
   return (
     <div>
-      <form className='loginform' onSubmit={handleSubmit}>
-        <label>username_or_Email</label>
-        <input type='text' name = "username_or_Email" id = "username_or_Email" value={formData.username_or_Email} onChange={handleChange} required ></input><br />
-        <label>Password</label>
-        <input type='text' name = "password" id = "password" value={formData.password} onChange={handleChange} required ></input>
-        <input type='submit' name='submit' id='submit'></input>
-      </form>
+    
+    <section>
+      <div className="imgBxLogin">
+          <img src={logImg} alt="Form" />
+      </div>
+      <div className="contentBxLogin">
+
+       <form className="formLogin" onSubmit={handleSubmit}>
+        <p className="form-heading">Login</p>
+        
+
+        
+
+          <div className="inputBx">
+          <input
+            type="text"
+            id='email'
+            name="email"
+            value={formData.username_or_Emailemail} onChange={handleChange}
+            className="input-box"
+            placeholder="Email"
+            required
+          />
+          </div>
+
+
+          
+          <div className="inputBx">
+          <input
+            type="password"
+            id='password'
+            name="password"
+            value={formData.password} onChange={handleChange}
+            className="input-box"
+            placeholder="Password"
+            required
+          />
+          </div>
+
+          
+          <div className="inputBx">
+          <input
+            type="submit"
+            name="sumbit-btn"
+            value="Login"
+            className="submit-button"
+          />
+          </div>
+          <div className="backSign">
+            Don't have an account? <Link to="/register" className='backSignLink'>Sign up</Link>
+          </div>
+        </form>
+      </div>
+     </section>
     </div>
-  )
+  );
 }
 
 export default Login
