1b18e865dc7c6b00b78250d264d2bca955903ab8
diveshkar <divesh.kar@gmail.com>
2023-07-10 01:07:23 +0530

    change payment

--- frontend/src/Component/Payment.js
+++ frontend/src/Component/Payment.js
@@ -2,21 +2,26 @@ import React from 'react'
 import './payment.css'
 
 const Payment = () => {
-  return (
-    <div>
-      <div className="content-center-Bx">
-        <h2 className='text-heading'>Payment Details :</h2>
-        <p className="text-content">Account Holder Name : ITUM</p>
-        <p className="text-content">Account No : 28XXXXXXXX</p>
-        <p className="text-content">Bank Name : BOC</p>
-        <p className="text-content">Branch Name : Diyagama</p>
-        <form action="" className="upload">
-            <input className="file" type="file" id="myFile" name="filename"/>
-            <input className="btn-file" type="submit"/>
-        </form>
+  if(document.cookie.includes('sessionToken')){
+    return (
+      <div>
+        <div className="content-center-Bx">
+          <h2 className='text-heading'>Payment Details :</h2>
+          <p className="text-content">Account Holder Name : ITUM</p>
+          <p className="text-content">Account No : 28XXXXXXXX</p>
+          <p className="text-content">Bank Name : BOC</p>
+          <p className="text-content">Branch Name : Diyagama</p>
+          <form action="" className="upload">
+              <input className="file" type="file" id="myFile" name="filename"/>
+              <input className="btn-file" type="submit"/>
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }else{
+    window.location.href = '/home';
+  }
+  
 }
 
 export default Payment
